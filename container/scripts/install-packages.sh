2ab3b9a50c86034db9c031d65f0e29d699a24475
Mountain Star <github@mtnstar.net>
2026-01-14 19:12:28 +0000

    Add kubectl

--- container/scripts/install-packages.sh
+++ container/scripts/install-packages.sh
@@ -30,6 +30,13 @@ npm install -g prettier
 # Install Bitwarden CLI
 npm install -g @bitwarden/cli
 
+# Install latest kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+rm kubectl kubectl.sha256
+
 # cleanup to reduce image size
 apt-get clean
 rm -rf /var/lib/apt/lists/*
