9066f45a24565656f052367f910d31616e67f5f4
Mountain Star <github@mtnstar.net>
2026-03-04 13:54:47 +0000

    Upgrade ansible and dependencies

--- container/scripts/install-ansible.sh
+++ container/scripts/install-ansible.sh
@@ -2,10 +2,10 @@
 set -euo pipefail
 
 python3 -m venv /opt/python-venv
-pip install --no-cache-dir --upgrade pip==24.0 \
-  molecule==25.12.0 \
-  ansible==13.1.0 \
-  kubernetes==34.1.0 \
+pip install --no-cache-dir --upgrade pip==26.0.1 \
+  molecule==26.2.0 \
+  ansible==13.4.0 \
+  kubernetes==35.0.0 \
   pytest \
   testinfra \
   yamllint \
