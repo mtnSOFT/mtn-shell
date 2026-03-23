7eb1e08e408ea82304a9b01e89e58ac00fc8906f
Mountain Star <github@mtnstar.net>
2026-03-23 15:27:33 +0000

    Update molecule

--- container/scripts/install-ansible.sh
+++ container/scripts/install-ansible.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 python3 -m venv /opt/python-venv
 pip install --no-cache-dir --upgrade pip==26.0.1 \
-  molecule==26.2.0 \
+  molecule==26.3.0 \
   ansible==13.4.0 \
   kubernetes==35.0.0 \
   pytest \
